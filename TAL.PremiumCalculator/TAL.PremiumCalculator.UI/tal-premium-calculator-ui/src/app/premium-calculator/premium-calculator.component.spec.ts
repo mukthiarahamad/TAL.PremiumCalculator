a27941452afd81eeddd14fce693600494768a6ad
mukthiar shaik <mukthiar.ahamad@gmail.com>
2021-02-07 21:09:29 +1100

    Added the premium component usign the angular cli

---
+++ TAL.PremiumCalculator/TAL.PremiumCalculator.UI/tal-premium-calculator-ui/src/app/premium-calculator/premium-calculator.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PremiumCalculatorComponent } from './premium-calculator.component';
+
+describe('PremiumCalculatorComponent', () => {
+  let component: PremiumCalculatorComponent;
+  let fixture: ComponentFixture<PremiumCalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PremiumCalculatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PremiumCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
