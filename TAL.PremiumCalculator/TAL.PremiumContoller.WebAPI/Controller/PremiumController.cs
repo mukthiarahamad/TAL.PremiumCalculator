547ac768cc9551d95b63c1b845ba8a0697893d99
mukthiar shaik <mukthiar.ahamad@gmail.com>
2021-02-07 19:07:48 +1100

    Created a PremiumController for the Web api project

---
+++ TAL.PremiumCalculator/TAL.PremiumContoller.WebAPI/Controller/PremiumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TAL.PremiumCalculator.BLL.Interface;
+using TAL.PremiumCalculator.BLL.Models;
+using TAL.PremiumCalculator.BLL.Service;
+
+namespace TAL.PremiumContoller.WebAPI.Controller
+{
+
+    public class PremiumController : ApiController
+    {
+        IOccupationBLL _occupationBLL;
+        IPremiumCalculatorBLL _premiumCalculatorBLL;
+
+        public PremiumController(IOccupationBLL occupationBLL, IPremiumCalculatorBLL premiumCalculatorBLL)
+        {
+            _occupationBLL = occupationBLL;
+            _premiumCalculatorBLL = premiumCalculatorBLL;
+        }
+    }
+}
